f65c1376f0a8457b8e537fb9f4d2c506a7cdb2c8
jakeabellalearning <mimemko@gmail.com>
2019-11-16 02:56:24 +0800

    x

--- python-api-tutorial/helloworld.py
+++ python-api-tutorial/helloworld.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+def otherhello():
+    return 'Other Hello';
+
+@app.route('/hello/<name>')
+def hello(name):
+    return 'Hello : ' + name;
+
+def hellox(param):
+    return 'Hellox :' + param
+
+
+app.add_url_rule('/otherhello','otherhello',otherhello)
+app.add_url_rule('/hellox/<param>','hellox',hellox)
 if __name__ == '__main__':
-    app.run();
+    #app.run();
+    app.run(debug=True);
 
